e7e9e74e785932cc04300ed87b960d6e56dbab7e
davemar42 <86600161+davemar42@users.noreply.github.com>
2021-08-01 13:28:22 +0200

    Add files via upload

---
+++ n2.js
@@ -0,0 +1,32 @@
+const app=new Vue({
+    el:"#app",
+
+    data:{
+        titula:"esto es un pequeño ejemplo",
+        piezas:[
+            {descripcion:'memoria',capacidad:'8gb',precio:35, stock:3},
+            {descripcion:'hd',capacidad:'256gb',precio:75, stock:6},
+            {descripcion:'hd',capacidad:'512gbgb',precio:100, stock:0},
+            {descripcion:'motherboard',capacidad:'8gb',precio:35, stock:3},
+            {descripcion:'memoria',capacidad:'16gb',precio:70, stock:1},
+            {descripcion:'memoria',capacidad:'16gb',precio:75, stock:6},
+        ],
+        descrp:'',
+        
+        
+    
+
+        
+
+    },
+    methods:{
+
+        Buscarpieza(){
+           
+        }
+
+    }
+
+})
+    
+
